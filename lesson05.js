c803a6c964190e7f7d1b7349b4bc1ae987db9d60
Michael Clautice <michaelclautice@Michaels-MacBook-Pro-2.local>
2020-07-31 17:29:26 -0400

    Mike created lesson05.js

---
+++ lesson05.js
@@ -0,0 +1,41 @@
+
+// Lesson 05
+// NAMED FUNCTION DEFINITION
+function sayHi(){
+    console.log("++++++++++++++++++");
+    console.log("Aloha, from Maui!");
+    console.log("++++++++++++++++++");
+};
+// invoke-run-call the function
+sayHi();
+// ANONYMOUS FUNCTION DEFINITION EXPRESSION
+const sayBye = function(){
+    console.log("•••••••••••••••••••");
+    console.log("Good Bye, to You!");
+    console.log("•••••••••••••••••••");
+};
+// invoke-run the function
+sayBye();
+// CALLBACK FUNCTION DEFINITION
+// function param is callback function
+// function runs callback function
+// remember: param will b replaced w arg
+function firstFunction(paramForCallbackFunction){
+    paramForCallbackFunction();
+};
+// function expression definition of callback function here
+var tellTheTruth = function(){
+    var today = new Date();
+    console.log(today);
+    global.console.log('MIKE IS THE BESTEST!');
+    console.log("•••••••••••••••••••");
+    console.log("cartoon face:");
+    console.log("----------");
+    console.log("--O---O---");
+    console.log("----•-----");
+    console.log("-+++++++--");
+    console.log("----------");
+};
+// invoke-run-call the 1st function
+// send the callback function as arg
+firstFunction(tellTheTruth);
