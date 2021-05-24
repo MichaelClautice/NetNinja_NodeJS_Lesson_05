2a7a01ad17505b44a1270df79b66b054c8b88e60
Michael Clautice <michaelclauticeusa@gmail.com>
2021-05-24 13:32:39 -0400

    That Mike guy modified app.js Mon.5.24.21.

--- app.js
+++ app.js
@@ -3,24 +3,27 @@
 // The Net Ninja - NodeJS Tutorial for Beginners
 // Lesson 05 - Function Expressions
 //-------
+
 // NAMED FUNCTION DEFINITION
 function sayHi(){
-    console.log("\n\n++++++++++++++++++");
+    console.log("\n++++++++++++++++++");
     console.log("Aloha, from Maui!");
     console.log("++++++++++++++++++");
 };
 // invoke-run-call the function
 sayHi();
 //-------
+
 // ANONYMOUS FUNCTION DEFINITION EXPRESSION
 const sayBye = function(){
-    console.log("\n\n•••••••••••••••••••");
+    console.log("\n•••••••••••••••••••");
     console.log("Good Bye, to You!");
-    console.log("•••••••••••••••••••");
+    console.log("•••••••••••••••••••\n");
 };
 // invoke-run the function
 sayBye();
 //-------
+
 // CALLBACK FUNCTION DEFINITION
 // function param is callback function
 // function runs callback function
@@ -32,14 +35,15 @@ function firstFunction(paramForCallbackFunction){
 var tellTheTruth = function(){
     var today = new Date();
     console.log(today);
+    console.log("\n•••••••••••••••••••");
     global.console.log('MIKE IS THE BESTEST!');
-    console.log("\n\n•••••••••••••••••••");
-    console.log("cartoon face:");
+    console.log("•••••••••••••••••••");
+    console.log("\ncartoon face:");
     console.log("----------");
     console.log("--O---O---");
     console.log("----•-----");
     console.log("-+++++++--");
-    console.log("----------");
+    console.log("----------\n");
 };
 // invoke-run-call the 1st function
 // send the callback function as arg
